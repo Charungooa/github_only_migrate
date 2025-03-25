b7c6004cff8e3fabb3d98528aacf7f6a698bdd11
rishi <rishi@POCDomain.local>
2025-03-25 13:25:42 +0000

    updated new-resources.tf file and varibale and provider

--- provider.tf
+++ provider.tf
@@ -8,23 +8,23 @@ terraform {
       source  = "gitlabhq/gitlab"
       version = "~> 17.0"
     }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
+    # azurerm = {
+    #   source  = "hashicorp/azurerm"
+    #   version = "~> 3.0"
+    # }
     null = {
       source  = "hashicorp/null"
       version = "~> 3.0"
     }
   }
   
-  backend "azurerm" {
-    resource_group_name  = "Backend-rg"
-    storage_account_name = "backednstg756"
-    container_name       = "newbackend-container"
-    key                  = "secmigrate.tfstate"
-    # key is provided dynamically via -backend-config in the workflow
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "Backend-rg"
+  #   storage_account_name = "backednstg756"
+  #   container_name       = "newbackend-container"
+  #   key                  = "secmigrate.tfstate"
+  #   # key is provided dynamically via -backend-config in the workflow
+  # }
   
   # The backend "azurerm" block has been removed entirely
 }
@@ -39,10 +39,10 @@ provider "gitlab" {
   base_url = "https://gitlab.com"
 }
 
-provider "azurerm" {
-  features {}
-  client_id       = var.AZURE_CLIENT_ID
-  client_secret   = var.AZURE_CLIENT_SECRET
-  subscription_id = var.AZURE_SUBSCRIPTION_ID
-  tenant_id       = var.AZURE_TENANT_ID
-}
+# provider "azurerm" {
+#   features {}
+#   # client_id       = var.AZURE_CLIENT_ID
+#   # client_secret   = var.AZURE_CLIENT_SECRET
+#   # subscription_id = var.AZURE_SUBSCRIPTION_ID
+#   # tenant_id       = var.AZURE_TENANT_ID
+# }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,12 +1,12 @@
 gitlab_project_path = "torque4/lintingapp"
 github_owner        = "charungooa"
-github_repo_name    = "az-vnet"
-key_vault_name_prod = null
-key_vault_name_dev  = "dev-apollo-keyvault-05"
-key_vault_name_staging = null
-azure_resource_group_prod   = null
-azure_resource_group_staging = null
-azure_resource_group_dev    = "backend-rg"
+github_repo_name    = "py-app-repo"
+# key_vault_name_prod = null
+# key_vault_name_dev  = "dev-apollo-keyvault-05"
+# key_vault_name_staging = null
+# azure_resource_group_prod   = null
+# azure_resource_group_staging = null
+# azure_resource_group_dev    = "backend-rg"
 
 #gitlab_token        = "glpat-xxxxxxxxxxxxxxxxx"
 #github_token        = "ghp_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
