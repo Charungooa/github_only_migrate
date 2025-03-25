d6275fcfe642326822eb20056ef31a743384d650
Amit Charungoo <v-2amitc@hotmail.com>
2025-03-25 17:44:05 +0530

    updated files for new requirement

--- new_resources.tf
+++ new_resources.tf
@@ -1,9 +1,3 @@
-# Key Vault Data Source (Development Only)
-data "azurerm_key_vault" "dev_vault" {
-  name                = var.key_vault_name_dev
-  resource_group_name = var.azure_resource_group_dev
-}
-
 # GitLab Project and Variables Data Sources
 data "gitlab_project" "project" {
   path_with_namespace = var.gitlab_project_path
@@ -28,8 +22,6 @@ resource "github_repository_environment" "environments" {
 
 # Local Variables
 locals {
-  repo_suffix = upper(replace(var.github_repo_name, "/[^a-zA-Z0-9-]/", "-"))
-
   # Map GitLab environment scopes to GitHub environment names with aliases
   github_env_mapping = {
     "master"      = "master"
@@ -40,62 +32,37 @@ locals {
     "dev"         = "development"
   }
 
-  # Masked secrets: Only include those explicitly scoped to development or dev
-  masked_secrets = {
-    for v in data.gitlab_project_variables.secrets.variables :
-    "development-${v.key}" => v
-    if v.masked && (lower(v.environment_scope) == "development" || lower(v.environment_scope) == "dev")
-  }
-
-  # Unmasked secrets for All(default), i.e., environment_scope == "*"
-  all_default_unmasked_secrets = {
+  # All variables for repository level (environment_scope == "*")
+  all_default_secrets = {
     for v in data.gitlab_project_variables.secrets.variables :
     v.key => v
-    if !v.masked && v.environment_scope == "*" && v.value != "" && v.value != null
+    if v.environment_scope == "*" && v.value != "" && v.value != null
   }
 
-  # Unmasked secrets for specific environments, i.e., environment_scope != "*" and not null
-  env_specific_unmasked_secrets = {
+  # All variables for specific environments (environment_scope != "*")
+  env_specific_secrets = {
     for v in data.gitlab_project_variables.secrets.variables :
     "${lower(v.environment_scope)}-${v.key}" => v
-    if !v.masked && v.environment_scope != "*" && v.environment_scope != null && v.value != "" && v.value != null
+    if v.environment_scope != "*" && v.environment_scope != null && v.value != "" && v.value != null
   }
 }
 
-# Store Masked Variables in Development Key Vault
-resource "azurerm_key_vault_secret" "masked_secrets" {
-  for_each = local.masked_secrets
+# Store All (default) Variables as GitHub Repository Secrets
+resource "github_actions_secret" "all_default_secrets" {
+  for_each = local.all_default_secrets
 
-  name         = "${local.repo_suffix}-${replace(replace(each.value.key, "_", "-"), "/[^a-zA-Z0-9-]/", "-")}"
-  value        = each.value.value
-  key_vault_id = data.azurerm_key_vault.dev_vault.id
-  content_type = var.github_repo_name
-  tags = {
-    environment = "development"
-    repository  = local.repo_suffix
-  }
-
-  lifecycle {
-    ignore_changes = [value]
-  }
+  repository       = data.github_repository.repo.name
+  secret_name      = each.value.key
+  plaintext_value  = each.value.value
 }
 
-# Store All(default) Unmasked Variables as GitHub Repository Variables
-resource "github_actions_variable" "all_default_vars" {
-  for_each = local.all_default_unmasked_secrets
+# Store Environment-Specific Variables as GitHub Environment Secrets
+resource "github_actions_environment_secret" "env_specific_secrets" {
+  for_each = local.env_specific_secrets
 
-  repository    = data.github_repository.repo.name
-  variable_name = each.value.key
-  value         = each.value.value
-}
-
-# Store Environment-Specific Unmasked Variables in GitHub Environments
-resource "github_actions_environment_variable" "env_specific_vars" {
-  for_each = local.env_specific_unmasked_secrets
-
-  repository    = data.github_repository.repo.name
-  environment   = local.github_env_mapping[element(split("-", each.key), 0)]
-  variable_name = each.value.key
-  value         = each.value.value
-  depends_on    = [github_repository_environment.environments]
+  repository       = data.github_repository.repo.name
+  environment      = local.github_env_mapping[element(split("-", each.key), 0)]
+  secret_name      = each.value.key
+  plaintext_value  = each.value.value
+  depends_on       = [github_repository_environment.environments]
 }
--- variable.tf
+++ variable.tf
@@ -13,49 +13,6 @@ variable "github_repo_name" {
   type        = string
 }
 
-# Resource group variables for each environment
-variable "azure_resource_group_prod" {
-  description = "Name of Azure resource group containing the production Key Vault"
-  type        = string
-  default     = "Backend-rg"
-}
-
-variable "azure_resource_group_staging" {
-  description = "Name of Azure resource group containing the staging Key Vault"
-  type        = string
-  default     = "Backend-rg"
-}
-
-variable "azure_resource_group_dev" {
-  description = "Name of Azure resource group containing the development Key Vault"
-  type        = string
-  default     = "Backend-rg"
-}
-
-variable "key_vault_name_prod" {
-  description = "Name of existing Azure Key Vault for production"
-  type        = string
-  default     = "gitlabmigvault"
-}
-
-variable "key_vault_name_dev" {
-  description = "Name of existing Azure Key Vault for development"
-  type        = string
-  default     = "gitlabmigvault"
-}
-
-variable "key_vault_name_staging" {
-  description = "Name of existing Azure Key Vault for staging"
-  type        = string
-  default     = "null"
-}
-
-variable "azure_location" {
-  description = "Azure region for resources"
-  type        = string
-  default     = "eastus"
-}
-
 variable "gitlab_token" {
   description = "GitLab Personal Access Token for API access"
   type        = string
@@ -66,36 +23,4 @@ variable "github_token" {
   description = "GitHub Personal Access Token"
   type        = string
   sensitive   = true
-}
-
-variable "AZURE_TENANT_ID" {
-  description = "Azure AD Tenant ID"
-  type        = string
-}
-
-variable "AZURE_CLIENT_OBJECT_ID" {
-  description = "Object ID for Azure AD principal with Key Vault access"
-  type        = string
-}
-
-variable "AZURE_CLIENT_ID" {
-  type        = string
-  description = "Azure AD Application client ID"
-}
-
-variable "AZURE_CLIENT_SECRET" {
-  type        = string
-  description = "Azure AD Application client secret"
-  sensitive   = true
-}
-
-variable "AZURE_SUBSCRIPTION_ID" {
-  type        = string
-  description = "Azure subscription ID"
-}
-
-variable "storage_account_name" {
-  type        = string
-  description = "Azure Storage Account name"
-  default     = "backednstg755"
 }
